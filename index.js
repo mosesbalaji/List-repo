603f01b3be8eaf80d310c3b216515435013abc7d
mosesbalaji <moses.balaji-external@tcs.com>
2022-01-06 15:03:12 +0800

    List repo

--- index.js
+++ index.js
@@ -13,4 +13,30 @@ async function run() {
     , organizationinp = getRequiredInput('organization')
     , maxRetries = getRequiredInput('octokit_max_retries')
   ;
+  let regex = /^[\w\.\_\-]+((,|-)[\w\.\_\-]+)*[\w\.\_\-]+$/g;
+  let validate_org = regex.test(organizationinp);
+  if((!validate_org)) {
+    throw new Error('Provide a valid organization - It accept only comma separated value');
+  }
+
+  let sinceregex = /^(20)\d\d-(0[1-9]|1[012])-([012]\d|3[01])T([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/ 
+;
+
+await io.mkdirP(outputDir)
+
+  const octokit = githubClient.create(token, maxRetries)
+    , orgActivity = new OrganizationActivity(octokit)
+  ;
+
+  //***start */
+  let organizationlist = organizationinp.split(',');
+  let removeMulUserList = [];
+  let jsonfinallist = [];
+  let rmvconfrm = 0;
+  for(const organization of organizationlist){
+    console.log(`Attempting to generate ${organization} - user activity data, this could take some time...`);
+    const orgsComments = await orgActivity.listForOrg(organization);
+      console.log(orgsComments)
+    
+  }
 }
